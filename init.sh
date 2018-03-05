f75482553134b31dba0bf738770db7e5f3ae65aa
RekGRpth <rekgrpth@gmail.com>
2018-03-05 12:51:26 +0500

    up

--- init.sh
+++ init.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-if [ ! -d /home/user/web2py ]; then
-    cd /home/user && git clone --recursive https://github.com/web2py/web2py.git
-    cd /home/user/web2py && python -c "from gluon.main import save_password; save_password('$PASSWORD', 4444)"
-else
-    cd /home/user/web2py && git pull --recursive
-fi
+test ! -d /home/user/web2py && cd /home/user && git clone --recursive https://github.com/web2py/web2py.git && cd /home/user/web2py && python -c "from gluon.main import save_password; save_password('$PASSWORD', 4444)"
