02181dacc1f33e299b72b151ff8b08e057f60c5c
RekGRpth <rekgrpth@gmail.com>
2018-03-13 12:06:03 +0500

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-groupmod --gid "$GROUP_ID" user && \
-usermod  --uid "$USER_ID" user && \
-chown --recursive "$USER_ID":"$GROUP_ID" "$HOME" && \
-sed -i "s|^user www-data;$|user user;|gi" "/etc/nginx/nginx.conf" && \
-sed -i "s|^worker_processes auto;$|worker_processes $PROCESSES;|gi" "/etc/nginx/nginx.conf" && \
-sed -i "s|^processes = 4$|processes = $PROCESSES|gi" "/etc/uwsgi/apps-enabled/uwsgi.ini" && \
+groupmod --gid "$GROUP_ID" user
+usermod  --uid "$USER_ID" user
+chown --recursive "$USER_ID":"$GROUP_ID" "$HOME"
+sed -i "s|^user www-data;$|user user;|gi" "/etc/nginx/nginx.conf"
+if [ "$PROCESSES" != "auto" ]; then
+    sed -i "s|^worker_processes auto;$|worker_processes $PROCESSES;|gi" "/etc/nginx/nginx.conf"
+    sed -i "s|^processes = 4$|processes = $PROCESSES|gi" "/etc/uwsgi/apps-enabled/uwsgi.ini"
+fi
 exec "$@"
