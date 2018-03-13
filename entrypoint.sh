94aa533ce9713da257131b8e5a2ee3057f9b20a7
RekGRpth <rekgrpth@gmail.com>
2018-03-13 12:08:21 +0500

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,7 @@ sed -i "s|^user www-data;$|user user;|gi" "/etc/nginx/nginx.conf"
 if [ "$PROCESSES" != "auto" ]; then
     sed -i "s|^worker_processes auto;$|worker_processes $PROCESSES;|gi" "/etc/nginx/nginx.conf"
     sed -i "s|^processes = 4$|processes = $PROCESSES|gi" "/etc/uwsgi/apps-enabled/uwsgi.ini"
+else
+    sed -i "s|^processes = 4$|processes = $(nproc --all)|gi" "/etc/uwsgi/apps-enabled/uwsgi.ini"
 fi
 exec "$@"
