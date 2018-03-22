013eb214d518f52d38e1a9c0f2c8efa663ec0ece
RekGRpth <rekgrpth@gmail.com>
2018-03-22 10:04:08 +0500

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-if [ "$GROUP_ID" = "" ]; then GROUP_ID=$(id -g $GROUP); fi
-if [ "$GROUP_ID" != "$(id -g $GROUP)" ]; then
-    find / -group $GROUP -exec chgrp "$GROUP_ID" {} \;
-    groupmod --gid "$GROUP_ID" $GROUP
+if [ "$GROUP_ID" = "" ]; then GROUP_ID=$(id -g "$GROUP"); fi
+if [ "$GROUP_ID" != "$(id -g "$GROUP")" ]; then
+    find / -group "$GROUP" -exec chgrp "$GROUP_ID" {} \;
+    groupmod --gid "$GROUP_ID" "$GROUP"
 fi
 
-if [ "$USER_ID" = "" ]; then USER_ID=$(id -u $USER); fi
-if [ "$USER_ID" != "$(id -u $USER)" ]; then
-    find / -user $USER -exec chown "$USER_ID" {} \;
-    usermod --uid "$USER_ID" $USER
+if [ "$USER_ID" = "" ]; then USER_ID=$(id -u "$USER"); fi
+if [ "$USER_ID" != "$(id -u "$USER")" ]; then
+    find / -user "$USER" -exec chown "$USER_ID" {} \;
+    usermod --uid "$USER_ID" "$USER"
 fi
+
+find "$HOME" ! -group "$GROUP" -exec chgrp "$GROUP_ID" {} \;
+find "$HOME" ! -user "$USER" -exec chown "$USER_ID" {} \;
 
 exec su-exec "$USER" "$@"
