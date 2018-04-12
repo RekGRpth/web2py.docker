1d0bf513c7e0c554c4550501bf19afa43f8765ba
RekGRpth <rekgrpth@gmail.com>
2018-04-12 13:32:50 +0500

    up

--- font.sh
+++ font.sh
@@ -47,5 +47,8 @@ done
 
 grep -r "DEFAULT_CSS = \"\"\"" /usr/lib/python3.6/site-packages/reportlab /usr/lib/python3.6/site-packages/xhtml2pdf | cut -d ':' -f 1 | sort -u | while read -r FILE; do
     echo "$FILE"
-    sed -i "/^DEFAULT_CSS/cfrom os import path, listdir\ndejavu = '/usr/share/fonts/ttf-dejavu'\nfonts = {file.split('.')[0]: path.join(dejavu, file) for file in listdir(dejavu) if file.endswith('.ttf')}\nDEFAULT_CSS = '\\\n'.join(('@font-face { font-family: %s; src: url(\"%s\"); }' % (name, file) for name, file in fonts.items())) + \"\"\"" "$FILE"
+    sed -i "s|font-weight:bold;|font-weight:bold;font-family: DejaVuSans-Bold;|g" "$FILE"
+    sed -i "s|font-weight: bold;|font-weight:bold;font-family: DejaVuSans-Bold;|g" "$FILE"
+    sed -i "s|font-style: italic;|font-style: italic;font-family: DejaVuSans-Oblique;|g" "$FILE"
+    sed -i "/^DEFAULT_CSS/cfrom os import path, listdir\ndejavu = '/usr/share/fonts/ttf-dejavu'\nfonts = {file.split('.')[0]: path.join(dejavu, file) for file in listdir(dejavu) if file.endswith('.ttf')}\nDEFAULT_CSS = '\\\n'.join(('@font-face { font-family: \"%s\"; src: \"%s\";%s%s }' % (name, file, ' font-weight: \"bold\";' if 'bold' in name.lower() else '', ' font-style: \"italic\";' if 'italic' in name.lower() or 'oblique' in name.lower() else '') for name, file in fonts.items())) + \"\"\"" "$FILE"
 done
