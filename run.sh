f9bcadc32803d779a921ccf296eba1dbbcde23b9
RekGRpth <rekgrpth@gmail.com>
2018-09-18 08:41:35 +0500

    up

--- run.sh
+++ run.sh
@@ -17,8 +17,23 @@ docker run \
     --detach \
     --env USER_ID=$(id -u) \
     --env GROUP_ID=$(id -g) \
+    --hostname websocket \
+    --link postgres \
+    --name websocket \
+    --restart always \
+    --volume /etc/certs:/etc/certs:ro \
+    --volume web2py:/data \
+    rekgrpth/web2py su-exec uwsgi python gluon/contrib/websocket_messaging.py -k web2py -p 8888 -s /etc/certs/`hostname -d`.key -c /etc/certs/`hostname -d`.crt
+docker run \
+    --add-host `hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
+    --add-host web2py-`hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
+    --add-host websocket-`hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
+    --detach \
+    --env USER_ID=$(id -u) \
+    --env GROUP_ID=$(id -g) \
     --hostname web2py \
     --link postgres \
+    --link websocket \
     --name web2py \
     --restart always \
     --volume web2py:/data \
@@ -32,21 +47,8 @@ docker run \
     --env GROUP_ID=$(id -g) \
     --hostname scheduler \
     --link postgres \
+    --link websocket \
     --name scheduler \
     --restart always \
     --volume web2py:/data \
     rekgrpth/web2py su-exec uwsgi supervisord
-docker run \
-    --add-host `hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
-    --add-host web2py-`hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
-    --add-host websocket-`hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
-    --detach \
-    --env USER_ID=$(id -u) \
-    --env GROUP_ID=$(id -g) \
-    --hostname websocket \
-    --link postgres \
-    --name websocket \
-    --restart always \
-    --volume /etc/certs:/etc/certs:ro \
-    --volume web2py:/data \
-    rekgrpth/web2py su-exec uwsgi python gluon/contrib/websocket_messaging.py -k web2py -p 8888 -s /etc/certs/`hostname -d`.key -c /etc/certs/`hostname -d`.crt
