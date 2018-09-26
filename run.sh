869fc3fc2ae14707dddcdea634f9e907199aa654
RekGRpth <rekgrpth@gmail.com>
2018-09-26 08:45:08 +0500

    up

--- run.sh
+++ run.sh
@@ -12,41 +12,34 @@ docker pull rekgrpth/web2py || exit $?
 docker volume create web2py || exit $?
 docker network create my
 docker run \
-    --add-host `hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
-    --add-host web2py-`hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
-    --add-host websocket-`hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
     --detach \
     --env USER_ID=$(id -u) \
     --env GROUP_ID=$(id -g) \
     --hostname websocket \
+    --link nginx:web2py-$(hostname -f) \
     --name websocket \
     --network my \
     --restart always \
     --volume /etc/certs:/etc/certs \
     --volume web2py:/data \
     rekgrpth/web2py su-exec uwsgi python gluon/contrib/websocket_messaging.py -k web2py -p 8888
-#    rekgrpth/web2py su-exec uwsgi python gluon/contrib/websocket_messaging.py -k web2py -p 8888 -s /etc/certs/`hostname -d`.key -c /etc/certs/`hostname -d`.crt
 docker run \
-    --add-host `hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
-    --add-host web2py-`hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
-    --add-host websocket-`hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
     --detach \
     --env USER_ID=$(id -u) \
     --env GROUP_ID=$(id -g) \
     --hostname web2py \
+    --link nginx:web2py-$(hostname -f) \
     --name web2py \
     --network my \
     --restart always \
     --volume web2py:/data \
     rekgrpth/web2py
 docker run \
-    --add-host `hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
-    --add-host web2py-`hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
-    --add-host websocket-`hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
     --detach \
     --env USER_ID=$(id -u) \
     --env GROUP_ID=$(id -g) \
     --hostname scheduler \
+    --link nginx:web2py-$(hostname -f) \
     --name scheduler \
     --network my \
     --restart always \
