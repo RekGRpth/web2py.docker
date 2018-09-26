4bbbc3026f4e794893fd06ad1423579d7d3be0cf
RekGRpth <rekgrpth@gmail.com>
2018-09-26 10:19:35 +0500

    up

--- run.sh
+++ run.sh
@@ -4,25 +4,11 @@
 #docker push rekgrpth/web2py || exit $?
 docker stop web2py
 docker stop scheduler
-docker stop websocket
 docker rm web2py
 docker rm scheduler
-docker rm websocket
 docker pull rekgrpth/web2py || exit $?
 docker volume create web2py || exit $?
 docker network create my
-docker run \
-    --detach \
-    --env USER_ID=$(id -u) \
-    --env GROUP_ID=$(id -g) \
-    --hostname websocket \
-    --link nginx:web2py-$(hostname -f) \
-    --name websocket \
-    --network my \
-    --restart always \
-    --volume /etc/certs:/etc/certs \
-    --volume web2py:/data \
-    rekgrpth/web2py su-exec uwsgi python gluon/contrib/websocket_messaging.py -k web2py -p 8888
 docker run \
     --detach \
     --env USER_ID=$(id -u) \
