8dda1d30476c2fb2efcf74e65d21c071defdf934
RekGRpth <rekgrpth@gmail.com>
2018-09-26 15:11:13 +0500

    up

--- run.sh
+++ run.sh
@@ -14,6 +14,7 @@ docker run \
     --env GROUP_ID=$(id -g) \
     --env USER_ID=$(id -u) \
     --hostname web2py \
+    --link nginx:$(hostname -f) \
     --link nginx:web2py-$(hostname -f) \
     --name web2py \
     --network my \
@@ -25,6 +26,7 @@ docker run \
     --env GROUP_ID=$(id -g) \
     --env USER_ID=$(id -u) \
     --hostname scheduler \
+    --link nginx:$(hostname -f) \
     --link nginx:web2py-$(hostname -f) \
     --name scheduler \
     --network my \
