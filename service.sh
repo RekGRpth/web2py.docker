c308d5a7653735d4b8af33bf446f257f55601bf0
RekGRpth <rekgrpth@gmail.com>
2020-06-08 15:40:23 +0500

    up

--- service.sh
+++ service.sh
@@ -3,14 +3,16 @@
 #docker build --tag rekgrpth/web2py .
 #docker push rekgrpth/web2py
 docker pull rekgrpth/web2py
-docker volume create web2py
 docker network create --attachable --driver overlay docker || echo $?
+docker volume create web2py
+docker service rm web2py || echo $?
 docker service create \
-    --env GROUP_ID=$(id -g) \
+    --env GROUP_ID="$(id -g)" \
     --env LANG=ru_RU.UTF-8 \
     --env TZ=Asia/Yekaterinburg \
-    --env USER_ID=$(id -u) \
-    --hostname web2py \
+    --env USER_ID="$(id -u)" \
+    --hostname="{{.Service.Name}}-{{.Node.Hostname}}" \
+    --mode global \
     --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
     --mount type=bind,source=/run/postgresql,destination=/run/postgresql \
     --mount type=bind,source=/run/uwsgi,destination=/run/uwsgi \
