4a969b5fe7a4cb551b97515eb78e0e28bbc351be
RekGRpth <rekgrpth@gmail.com>
2021-07-30 14:05:31 +0500

    service

--- service.sh
+++ service.sh
@@ -1,10 +1,8 @@
-#!/bin/sh -ex
+#!/bin/sh -eux
 
-#docker build --tag rekgrpth/web2py .
-#docker push rekgrpth/web2py
-docker pull rekgrpth/web2py
-docker network create --attachable --driver overlay docker || echo $?
+docker pull ghcr.io/rekgrpth/web2py.docker
 docker volume create web2py
+docker network create --attachable --driver overlay docker || echo $?
 docker service rm web2py || echo $?
 docker service create \
     --env GROUP_ID="$(id -g)" \
@@ -20,18 +18,4 @@ docker service create \
     --mount type=volume,source=web2py,destination=/home \
     --name web2py \
     --network name=docker \
-    rekgrpth/web2py uwsgi --ini web2py.ini
-#docker service create \
-#    --env GROUP_ID=$(id -g) \
-#    --env LANG=ru_RU.UTF-8 \
-#    --env TZ=Asia/Yekaterinburg \
-#    --env USER_ID=$(id -u) \
-#    --hostname scheduler \
-#    --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
-#    --mount type=bind,source=/run/postgresql,destination=/run/postgresql \
-#    --mount type=bind,source=/run/uwsgi,destination=/run/uwsgi \
-#    --mount type=bind,source=/var/log/uwsgi/web2py,destination=/var/log/uwsgi \
-#    --mount type=volume,source=web2py,destination=/home \
-#    --name scheduler \
-#    --network name=docker \
-#    rekgrpth/web2py su-exec web2py python -m supervisor.supervisord --configuration /home/supervisord.conf
+    ghcr.io/rekgrpth/web2py.docker runsvdir /etc/service
