5c4c564a4805531306599fc376c47f4a7fafbb58
RekGRpth <rekgrpth@gmail.com>
2021-08-21 08:00:35 +0500

    up

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eux
 
-DOCKER_BUILDKIT=1 docker build --progress=plain --tag rekgrpth/web2py . 2>&1 | tee build.log
+DOCKER_BUILDKIT=1 docker build --progress=plain --tag ghcr.io/rekgrpth/web2py.docker . 2>&1 | tee build.log
