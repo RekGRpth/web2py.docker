efb4d3f1f6c5285feadead563f09b0fd26e39743
RekGRpth <rekgrpth@gmail.com>
2022-01-19 11:54:42 +0500

    fix

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -1,9 +1,15 @@
 #!/bin/sh -eux
 
-cd "${HOME}/src/pyhandlebars" && python3 setup.py build && python3 setup.py install --prefix /usr/local
-cd "${HOME}/src/pyhtmldoc" && python3 setup.py build && python3 setup.py install --prefix /usr/local
-cd "${HOME}/src/pymustach" && python3 setup.py build && python3 setup.py install --prefix /usr/local
-cd "${HOME}"
+cd "$HOME/src/pyhandlebars"
+python3 setup.py build
+python3 setup.py install --prefix /usr/local
+cd "$HOME/src/pyhtmldoc"
+python3 setup.py build
+python3 setup.py install --prefix /usr/local
+cd "$HOME/src/pymustach"
+python3 setup.py build
+python3 setup.py install --prefix /usr/local
+cd "$HOME"
 pip install --no-cache-dir --prefix /usr/local \
     captcha \
     client_bank_exchange_1c \
