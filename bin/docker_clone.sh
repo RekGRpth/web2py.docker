aefc1400ac58b6ed25c46ab0135dd60b391e9b09
RekGRpth <rekgrpth@gmail.com>
2022-02-26 09:33:02 +0500

    split

--- bin/docker_clone.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -eux
-
-mkdir -p "$HOME/src"
-cd "$HOME/src"
-git clone -b master https://github.com/RekGRpth/pyhandlebars.git
-git clone -b master https://github.com/RekGRpth/pyhtmldoc.git
-git clone -b master https://github.com/RekGRpth/pymustach.git
