e8e89a018821c0054272b3484fdecd4ba225b8f9
RekGRpth <rekgrpth@gmail.com>
2022-04-27 09:36:33 +0500

    rename

--- buildd.sh
+++ buildd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eux
 
-docker build --progress=plain --tag "ghcr.io/rekgrpth/web2py.docker:${INPUTS_BRANCH:-debian}" $(env | grep -E '^DOCKER_' | grep -v ' ' | sort -u | sed 's@^@--build-arg @g' | paste -s -d ' ') --file "${INPUTS_DOCKERFILE:-Dockerfile.debian}" . 2>&1 | tee build.log
+docker build --progress=plain --tag "ghcr.io/rekgrpth/web2py.docker:${INPUTS_BRANCH:-debian}" $(env | grep -E '^DOCKER_' | grep -v ' ' | sort -u | sed 's@^@--build-arg @g' | paste -s -d ' ') --file "${INPUTS_DOCKERFILE:-debian.Dockerfile}" . 2>&1 | tee build.log
--- buildo.sh
+++ buildo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eux
 
-docker build --progress=plain --tag "ghcr.io/rekgrpth/web2py.docker:${INPUTS_BRANCH:-oracle}" $(env | grep -E '^DOCKER_' | grep -v ' ' | sort -u | sed 's@^@--build-arg @g' | paste -s -d ' ') --file "${INPUTS_DOCKERFILE:-Dockerfile.oracle}" . 2>&1 | tee build.log
+docker build --progress=plain --tag "ghcr.io/rekgrpth/web2py.docker:${INPUTS_BRANCH:-oracle}" $(env | grep -E '^DOCKER_' | grep -v ' ' | sort -u | sed 's@^@--build-arg @g' | paste -s -d ' ') --file "${INPUTS_DOCKERFILE:-oracle.Dockerfile}" . 2>&1 | tee build.log
